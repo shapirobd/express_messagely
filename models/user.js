4374e7e0101b804a893e6080e765e2fac97e7fac
Brian D. Shapiro <briand.shapiro@Brians-MBP.attlocal.net>
2020-12-06 23:15:22 -0500

    /register route working

--- models/user.js
+++ models/user.js
@@ -1,61 +1,70 @@
 /** User class for message.ly */
-
-
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
 
 /** User of the site. */
 
 class User {
+	/** register new user -- returns
+	 *    {username, password, first_name, last_name, phone}
+	 */
 
-  /** register new user -- returns
-   *    {username, password, first_name, last_name, phone}
-   */
+	static async register({ username, password, first_name, last_name, phone }) {
+		const hashedPwd = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+		const join_at = new Date().toLocaleDateString();
+		const results = await db.query(
+			`INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING username, password, first_name, last_name, phone`,
+			[username, hashedPwd, first_name, last_name, phone, join_at]
+		);
+		return results.rows[0];
+	}
 
-  static async register({username, password, first_name, last_name, phone}) { }
+	/** Authenticate: is this username/password valid? Returns boolean. */
 
-  /** Authenticate: is this username/password valid? Returns boolean. */
+	static async authenticate(username, password) {}
 
-  static async authenticate(username, password) { }
+	/** Update last_login_at for user */
 
-  /** Update last_login_at for user */
+	static async updateLoginTimestamp(username) {}
 
-  static async updateLoginTimestamp(username) { }
+	/** All: basic info on all users:
+	 * [{username, first_name, last_name, phone}, ...] */
 
-  /** All: basic info on all users:
-   * [{username, first_name, last_name, phone}, ...] */
+	static async all() {}
 
-  static async all() { }
+	/** Get: get user by username
+	 *
+	 * returns {username,
+	 *          first_name,
+	 *          last_name,
+	 *          phone,
+	 *          join_at,
+	 *          last_login_at } */
 
-  /** Get: get user by username
-   *
-   * returns {username,
-   *          first_name,
-   *          last_name,
-   *          phone,
-   *          join_at,
-   *          last_login_at } */
+	static async get(username) {}
 
-  static async get(username) { }
+	/** Return messages from this user.
+	 *
+	 * [{id, to_user, body, sent_at, read_at}]
+	 *
+	 * where to_user is
+	 *   {username, first_name, last_name, phone}
+	 */
 
-  /** Return messages from this user.
-   *
-   * [{id, to_user, body, sent_at, read_at}]
-   *
-   * where to_user is
-   *   {username, first_name, last_name, phone}
-   */
+	static async messagesFrom(username) {}
 
-  static async messagesFrom(username) { }
+	/** Return messages to this user.
+	 *
+	 * [{id, from_user, body, sent_at, read_at}]
+	 *
+	 * where from_user is
+	 *   {id, first_name, last_name, phone}
+	 */
 
-  /** Return messages to this user.
-   *
-   * [{id, from_user, body, sent_at, read_at}]
-   *
-   * where from_user is
-   *   {id, first_name, last_name, phone}
-   */
-
-  static async messagesTo(username) { }
+	static async messagesTo(username) {}
 }
-
 
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
+const express = require("express");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
+const router = express.Router();
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +17,14 @@
  *
  *  Make sure to update their last-login!
  */
+router.post("/register", async (req, res, next) => {
+	try {
+		const user = await User.register(req.body);
+		const token = jwt.sign({ userame: user.username }, SECRET_KEY);
+		return res.json(token);
+	} catch (e) {
+		return next(e);
+	}
+});
+
+module.exports = router;
